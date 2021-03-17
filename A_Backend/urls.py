4dd408641e49e82cd03bd7561514a935e6ce1561
Bartosz Gardziel <01142169@pw.edu.pl>
2021-03-18 00:01:40 +0100

    Configured url paths and installed apps,updated .gitignore

--- A_Backend/urls.py
+++ A_Backend/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import BikeRentalApi.urls
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	path( 'admin/', admin.site.urls ),
+	path( 'api/', BikeRentalApi.urls )
 ]
