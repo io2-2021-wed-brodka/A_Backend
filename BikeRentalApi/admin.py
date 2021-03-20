61d51a5a097506cd2e6ca3839f6beeba21eac2af
bartosz <bartosz.g.1999@gmail.com>
2021-03-20 13:38:06 +0100

    Registered all models in admin site

--- BikeRentalApi/admin.py
+++ BikeRentalApi/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from BikeRentalApi.models import Bike, BikeStation, Reservation, Rental, Malfunction, User, Admin, Tech
+
+admin.site.register(Bike)
+admin.site.register(BikeStation)
+admin.site.register(Reservation)
+admin.site.register(Rental)
+admin.site.register(Malfunction)
+admin.site.register(Admin)
+admin.site.register(User)
+admin.site.register(Tech)
