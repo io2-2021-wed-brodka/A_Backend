07cc1a77d237936ce33e4d41e80782502c764062
Jan Kupis <janekkupis@gmail.com>
2021-03-24 22:55:00 +0100

    Add bike serializer

---
+++ BikeRentalApi/serializers/bikeSerializer.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from rest_framework.fields import IntegerField, ChoiceField, SerializerMethodField
+
+from BikeRentalApi.enums import BikeState
+from BikeRentalApi.models import Bike, User
+from .stationSerializer import StationSerializer
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class BikeSerializer(serializers.ModelSerializer):
+    id = IntegerField(label='ID', read_only=True)
+    bike_state = ChoiceField(choices=BikeState, required=True)
+    user = SerializerMethodField('get_user')
+    station = StationSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Bike
+        fields = '__all__'
+
+    def get_user(self, obj):
+        return UserSerializer(obj.rental_set.all().first().user).data if obj.rental_set.count() > 0 else None
