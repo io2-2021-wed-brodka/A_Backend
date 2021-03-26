78e2f322c5a1e77016ee18c13a5e6fa02791670e
Jan Kupis <janekkupis@gmail.com>
2021-03-26 11:34:18 +0100

    Fix indentation error

--- A_Backend/settings.py
+++ A_Backend/settings.py
@@ -80,12 +80,12 @@ DATABASES = {'default': dj_database_url.config()}
 if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'github_actions',
-           'USER': 'postgres',
-           'PASSWORD': 'postgres',
-           'HOST': '127.0.0.1',
-           'PORT': '5432',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github_actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
         }
     }
 
