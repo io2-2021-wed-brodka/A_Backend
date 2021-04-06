96a31d204c24d039235c15e6cb71b584c62a74dc
Jan Kupis <janekkupis@gmail.com>
2021-04-06 22:25:12 +0200

    Add WhiteNoise for serving static files

--- A_Backend/common_settings.py
+++ A_Backend/common_settings.py
@@ -25,6 +25,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'A_Backend.urls'
