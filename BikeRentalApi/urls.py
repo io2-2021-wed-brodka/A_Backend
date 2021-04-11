8b256c5115d70ddd8f7487127ddb6c27b40f2173
Paweł Dębski <01142162@pw.edu.pl>
2021-04-11 15:23:49 +0200

    Implemented list and detail 'bikes' endpoints

--- BikeRentalApi/urls.py
+++ BikeRentalApi/urls.py
@@ -1,2 +1,8 @@
+from django.urls import path
+
+from .views import bikes_list, bikes_detail
+
 urlpatterns = [
+    path('bikes/', bikes_list, name = 'bikes_list'),
+    path('bikes/<int:pk>', bikes_detail, name = 'bikes_detail')
 ]
--- BikeRentalApi/views.py
+++ BikeRentalApi/views.py
@@ -1,3 +1,50 @@
-# from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.parsers import JSONParser
+from rest_framework.exceptions import PermissionDenied, NotFound
 
-# Create your views here.
+from BikeRentalApi.models import Bike, Rental
+from BikeRentalApi.serializers.bikeSerializer import BikeSerializer
+from BikeRentalApi.authentication import authenticate
+from BikeRentalApi.enums import Role
+
+
+def bikes_list(request):
+    user = authenticate(request)
+
+    if request.method == 'GET':
+        if user.role < Role.Tech:
+            raise PermissionDenied()
+
+        bikes = Bike.objects.all()
+        serializer = BikeSerializer(bikes, many = True)
+        return JsonResponse(serializer.data, safe = False, status = 200)
+    elif request.method == 'POST':
+        if user.role != Role.Admin:
+            raise PermissionDenied()
+
+        data = JSONParser().parse(request)
+        serializer = BikeSerializer(data = data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status = 201)
+
+    raise NotFound()
+
+
+def bikes_detail(request, pk):
+    user = authenticate(request)
+
+    if request.method == 'DELETE':
+        if user.Role != Role.Admin:
+            raise PermissionDenied()
+
+        bike = get_object_or_404(Bike, pk = pk)
+        serializer = BikeSerializer(bike)
+
+        bike.delete()
+
+        return JsonResponse(serializer.data, status = 200)
+
+    raise NotFound()
