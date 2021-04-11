fac346ccc9ea601823cae0d7bebd8c8f77ed83b9
Paweł Dębski <01142162@pw.edu.pl>
2021-04-11 15:33:43 +0200

    Implemented list and detail 'stations' endpoints

--- BikeRentalApi/urls.py
+++ BikeRentalApi/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import bikes_list, bikes_detail
+from .views import bikes_list, bikes_detail, stations_list, station_detail
 
 urlpatterns = [
     path('bikes/', bikes_list, name = 'bikes_list'),
-    path('bikes/<int:pk>', bikes_detail, name = 'bikes_detail')
+    path('bikes/<int:pk>', bikes_detail, name = 'bikes_detail'),
+    path('stations/', stations_list, name = 'station_list'),
+    path('stations/<int:pk>', station_detail, name = 'station_detail')
 ]
--- BikeRentalApi/views.py
+++ BikeRentalApi/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import get_object_or_404
 from rest_framework.parsers import JSONParser
 from rest_framework.exceptions import PermissionDenied, NotFound
 
-from BikeRentalApi.models import Bike, Rental
+from BikeRentalApi.models import Bike, Rental, BikeStation
 from BikeRentalApi.serializers.bikeSerializer import BikeSerializer
+from BikeRentalApi.serializers.stationSerializer import StationSerializer
 from BikeRentalApi.authentication import authenticate
 from BikeRentalApi.enums import Role
 
@@ -42,8 +43,49 @@ def bikes_detail(request, pk):
 
         bike = get_object_or_404(Bike, pk = pk)
         serializer = BikeSerializer(bike)
-
         bike.delete()
+
+        return JsonResponse(serializer.data, status = 200)
+
+    raise NotFound()
+
+
+def stations_list(request):
+    user = authenticate(request)
+
+    if request.method == 'GET':
+        bikes = BikeStation.objects.all()
+        serializer = StationSerializer(bikes, many = True)
+        return JsonResponse(serializer.data, safe = False, status = 200)
+    elif request.method == 'POST':
+        if user.role != Role.Admin:
+            raise PermissionDenied()
+
+        data = JSONParser().parse(request)
+        serializer = StationSerializer(data = data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status = 201)
+
+    raise NotFound()
+
+
+def station_detail(request, pk):
+    user = authenticate(request)
+
+    if request.method == 'GET':
+        station = get_object_or_404(BikeStation, pk = pk)
+        serializer = StationSerializer(station)
+
+        return JsonResponse(serializer.data, status = 200)
+    elif request.method == 'DELETE':
+        if user.role != Role.Admin:
+            raise PermissionDenied()
+
+        station = get_object_or_404(BikeStation, pk = pk)
+        serializer = StationSerializer(station)
+        station.delete()
 
         return JsonResponse(serializer.data, status = 200)
 
