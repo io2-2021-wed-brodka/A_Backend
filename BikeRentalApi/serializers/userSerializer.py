5ea6d648bba8ceacc9745082c1403abc302532a2
Paweł Dębski <01142162@pw.edu.pl>
2021-04-12 14:25:58 +0200

    Updated user serializer

--- BikeRentalApi/serializers/userSerializer.py
+++ BikeRentalApi/serializers/userSerializer.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
+from rest_framework.fields import IntegerField, SerializerMethodField
 
-from BikeRentalApi.models import User
+from BikeRentalApi.models import AppUser
 
 
 class UserSerializer(serializers.ModelSerializer):
+    id = IntegerField(label = 'ID', read_only = True)
+    name = SerializerMethodField()
+
     class Meta:
-        model = User
-        fields = ['id', 'name']
+        model = AppUser
+        fields = ['name', 'id']
+
+    def get_name(self, obj):
+        return obj.user.first_name
