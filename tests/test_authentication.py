4954384635d6484ab604ce594935beb54ea9a66d
gardzielb <bartosz.g.1999@gmail.com>
2021-04-13 19:24:39 +0200

    Added more authenticate function tests

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,7 +1,7 @@
 import pytest
 from django.contrib.auth.models import User
-from django.test import RequestFactory
-from rest_framework.exceptions import PermissionDenied
+from rest_framework.test import APIRequestFactory
+from rest_framework.exceptions import PermissionDenied, NotAuthenticated, AuthenticationFailed
 
 from BikeRentalApi.authentication import authenticate_bikes_user
 from BikeRentalApi.models import Tech
@@ -18,14 +18,26 @@ class TestAuthentication:
         )
 
     @pytest.fixture
-    def authenticated_request(self, auth_user):
-        rq = RequestFactory().get('/api')
-        rq.headers = {'Authorization': f'Bearer {auth_user.username}'}
-        return rq
+    def unauthorized_request(self):
+        return APIRequestFactory().get('/api')
 
-    def test_raises_permission_denied_for_user_not_int_db(self, authenticated_request, auth_user):
+    @pytest.fixture
+    def authenticated_request(self, unauthorized_request, auth_user):
+        unauthorized_request.headers = {'Authorization': f'Bearer {auth_user.username}'}
+        return unauthorized_request
+
+    def test_raises_permission_denied_for_user_not_int_db(self, authenticated_request, unauthorized_request, auth_user):
         with pytest.raises(PermissionDenied):
             authenticate_bikes_user(authenticated_request)
+
+    def test_raises_not_authenticated_for_lacking_auth_header(self, unauthorized_request):
+        with pytest.raises(NotAuthenticated):
+            authenticate_bikes_user(unauthorized_request)
+
+    def test_raises_authentication_failed_for_invalid_auth_header(self, unauthorized_request):
+        unauthorized_request.headers = {'Authorization': 'invalid'}
+        with pytest.raises(AuthenticationFailed):
+            authenticate_bikes_user(unauthorized_request)
 
     def test_returns_authorized_user(self, authenticated_request, auth_user):
         tech = Tech.objects.create(user = auth_user)
