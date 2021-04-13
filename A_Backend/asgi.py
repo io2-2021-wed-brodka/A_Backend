7b938a5d7e900a6d6bda8d7a712580a22792286d
Jan Kupis <janekkupis@gmail.com>
2021-04-13 12:35:26 +0200

    Make other files aware of settings split

--- A_Backend/asgi.py
+++ A_Backend/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'A_Backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'A_Backend.development_settings')
 
 application = get_asgi_application()
