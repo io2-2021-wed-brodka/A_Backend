0eb9c111444d32a66e285f9f4ec12da0e58d7889
Artur Chmura <achmura66@gmail.com>
2021-04-17 19:42:25 +0200

    delete sqllite db

--- A_Backend/development_settings.py
+++ A_Backend/development_settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from .common_settings import * # noqa
-from .common_settings import BASE_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -23,10 +22,3 @@ SECRET_KEY = '6fvq0sccaann8jr)4dg16qzip$(xr#rrop$wu#1cbh7m-$n=c+'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
