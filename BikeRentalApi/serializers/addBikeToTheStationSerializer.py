df46050824327cd12ea000029a2f87b1d3344c67
Maciej Chlebny <chlebny59@gmail.com>
2021-04-20 12:35:29 +0200

    added some tests and changedsome errors

--- BikeRentalApi/serializers/addBikeToTheStationSerializer.py
+++ BikeRentalApi/serializers/addBikeToTheStationSerializer.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
+from rest_framework.fields import IntegerField
 
 from BikeRentalApi.models import BikeStation
 
 
 class AddBikeStationSerializer(serializers.ModelSerializer):
+    id = IntegerField()
+
     class Meta:
         model = BikeStation
         fields = ['id']
 
     def create(self, validated_data):
-        return BikeStation.objects.filter(**validated_data).first()
+        try:
+            return BikeStation.objects.get(id = validated_data['id'])
+        except BikeStation.DoesNotExist:
+            return None
