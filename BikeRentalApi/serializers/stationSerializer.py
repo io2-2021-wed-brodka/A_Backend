3e0e3dfc2359eeca0d7a601d8dbeb8d01b3a991d
Jan Kupis <janekkupis@gmail.com>
2021-05-13 23:44:36 +0200

    Simplify existing serializers

--- BikeRentalApi/serializers/stationSerializer.py
+++ BikeRentalApi/serializers/stationSerializer.py
@@ -7,15 +7,12 @@ from BikeRentalApi.models import BikeStation
 
 class StationSerializer(serializers.ModelSerializer):
     id = CharField()
-    status = SerializerMethodField()
+    status = CharField(source = 'state.label')
     activeBikesCount = SerializerMethodField()
 
     class Meta:
         model = BikeStation
         exclude = ['state']
-
-    def get_status(self, obj: BikeStation):
-        return obj.state.label
 
     def get_activeBikesCount(self, obj: BikeStation):
         return obj.bike_set.filter(bike_state = BikeState.Working).count()
