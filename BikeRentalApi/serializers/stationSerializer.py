1fbe83f8616e34a3614c07c86893f5e845643121
Paweł Dębski <52053209+debskipawel@users.noreply.github.com>
2021-05-23 20:54:09 +0200

    Added new migration and modified the Station serializers

--- BikeRentalApi/serializers/stationSerializer.py
+++ BikeRentalApi/serializers/stationSerializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework.fields import SerializerMethodField, CharField
+from rest_framework.fields import SerializerMethodField, CharField, IntegerField
 
 from BikeRentalApi.enums import BikeState
 from BikeRentalApi.models import BikeStation
@@ -9,10 +9,11 @@ class StationSerializer(serializers.ModelSerializer):
     id = CharField()
     status = CharField(source = 'state.label')
     activeBikesCount = SerializerMethodField()
+    bikesLimit = IntegerField(source = 'bikes_limit')
 
     class Meta:
         model = BikeStation
-        exclude = ['state']
+        exclude = ['state', 'bikes_limit']
 
     def get_activeBikesCount(self, obj: BikeStation):
         return obj.bike_set.filter(bike_state = BikeState.Working).count()
