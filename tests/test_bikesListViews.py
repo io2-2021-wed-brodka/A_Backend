2e736e568f7e0b1d694dfceed92a763d20f46f55
Paweł Dębski <52053209+debskipawel@users.noreply.github.com>
2021-06-01 21:35:23 +0200

    Added tests for new changes

--- tests/test_bikesListViews.py
+++ tests/test_bikesListViews.py
@@ -39,6 +39,10 @@ class TestBikesListViews:
         return BikeStation.objects.create(name = 'Test station', state = StationState.Working)
 
     @pytest.fixture
+    def station_full(self):
+        return BikeStation.objects.create(name = 'Test station', state = StationState.Working, bikes_limit = 0)
+
+    @pytest.fixture
     def bike(self, user, station):
         return Bike.objects.create(station = station, bike_state = BikeState.Working)
 
@@ -142,3 +146,13 @@ class TestBikesListViews:
 
         response = bikes_list(request)
         assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_post_bikes_list_full_station_status(self, factory, station_full, admin):
+        body = json.dumps({'stationId': station_full.pk})
+        request = factory.post('/bikes', content_type = 'application/json', data = body)
+        headers = {'Authorization': f'Bearer {admin.user.username}'}
+        headers.update(request.headers)
+        request.headers = headers
+
+        response = bikes_list(request)
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
